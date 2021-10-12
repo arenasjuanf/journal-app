85eee655649c5e1b09f9e341be6a84cde3cf3448
arenasjuanf <juanfa107@gmail.com>
2021-10-12 18:29:38 -0500

    Create NotesReducer

--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,18 @@
 export const types = {
     login: '[Auth] Login',
     logout: '[Auth] Logout',
+
     uiSetError: "[Ui] Set Error",
     uiRemoveError: "[Ui] Remove Error",
-    uiStartLoading: "[Ui] Start Loading",
-    uiFinishLoading: "[Ui] Finish Loading"
 
+    uiStartLoading: "[Ui] Start Loading",
+    uiFinishLoading: "[Ui] Finish Loading",
+
+    notesAddNew: '[Notes] New note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdated: '[Notes] Update note saved',
+    notesFileUrl: '[Notes] Updated image url ',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning: '[Notes] Logout cleaning',
 }
